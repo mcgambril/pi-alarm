b59b172c376ca4356baa06552a8a312a9e63c980
mcgambril <mcgambril@crimson.ua.edu>
2017-05-17 00:06:28 -0500

    Messing around with curl
    not really anything. Some basic things to test if curl works.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -13,7 +13,26 @@ class Home extends CI_Controller {
         parent::__construct();
     }
 
+
     public function index() {
+        //echo phpinfo();
+        //current api server is http://api.easybulb.com/v1/
+        //ctrl_box privte ip address 192.168.1.69
+        //ctrl_box mac address f0:fe:6b:14:b9:54
+
+        //https://www.robertprice.co.uk/robblog/usng_ph_to_send_a_udp_message-shtml/
+        //php.net/manual/en/function.socket-create.php
+        $ch = curl_init(); if($ch === false) {echo 'didnt work';} else {echo 'curl works';}
+        $ctrl_box = '192.168.1.69';
+        $port = 80;
+        $mac_addr = 'f0:fe:6b:14:b9:54';
+        $api_url = 'http://api.easybulb.com/v1/';
+        $on_msg = 'GET white/on/';
+
+        $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+        socket_sendto($socket, $on_msg, strlen($on_msg), 0, $ctrl_box, $port);
+        socket_close($socket);
+
         $this->load->view('home_view');
     }
 }
